aa3a34de214cda9c3d8a343a5a706b7453de1af6
roko bobic <roko.bobic@gmail.com>
2020-03-02 21:52:18 +0100

    style table of processes

--- web-ui/src/components/Processes.vue
+++ web-ui/src/components/Processes.vue
@@ -1,33 +1,51 @@
 <template>
-  <div>
-    <ul>
-       <li v-for="(process,index) in Processes" v-bind:key="process.Pid" >
-       {{index}} {{ process.Pid }}{{ process.Name }}{{ process.Status }}{{ process.CPUPercent }}{{ process.MemPercent }}{{ process.Username }}{{ process.Uptime }}
-      </li>
-    </ul>
-  </div>
+  <q-card flat bordered>
+    <q-card-section>
+      <strong class="text-h6">Processes</strong>
+    </q-card-section>
+
+    <q-card-section class="q-pt-none">
+      <q-table
+        style="height: 400px"
+        :data="Processes"
+        :columns="Columns"
+        row-key="Pid"
+        hide-bottom
+        :pagination.sync="pagination"
+        :rows-per-page-options="[0]"
+         :dense="$q.screen.lt.md"
+      />
+    </q-card-section>
+  </q-card>
 </template>
 
 <script>
-import {monitorService} from "../monitor.service"
+import { monitorService } from "../monitor.service";
 export default {
   name: "Processes",
   data() {
     return {
-        Processes:[]
-      };
-    },
- mounted() {
+      Processes: [],
+      pagination: {
+        page: 1,
+        rowsPerPage: 20
+      },
+      Columns: [
+        { name: "Pid", label: "Pid", field: "Pid" },
+        { name: "Name", label: "Name", field: "Name" },
+        { name: "CPUPercent", label: "CPU", field: "CPUPercent" },
+        { name: "MemPercent", label: "Mem", field: "MemPercent" },
+        { name: "Username", label: "Username", field: "Username" },
+        { name: "Uptime", label: "Uptime", field: "Uptime" }
+      ]
+    };
+  },
+  mounted() {
     var self = this;
     monitorService.GetListOfProcesses(function(evt) {
       var parsed = JSON.parse(evt.data);
       self.Processes = parsed;
     });
-    
-  },
-  
+  }
 };
-
-
-
 </script>
